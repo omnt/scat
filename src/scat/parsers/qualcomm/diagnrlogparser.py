07d54587a7b3bbe5b5ee83125a786781de6d39a3
Johann <johann.hackler@fokus.fraunhofer.de>
2024-06-11 13:45:00 +0200

    update

--- src/scat/parsers/qualcomm/diagnrlogparser.py
+++ src/scat/parsers/qualcomm/diagnrlogparser.py
@@ -75,14 +75,13 @@ class DiagNrLogParser:
                 self.parent.logger.log(logging.WARNING, 'Unknown NR ML1 Information packet, version: {}.{}'.format(pkt_ver.ml1_rel_maj, pkt_ver.ml1_rel_min))
                 self.parent.logger.log(logging.WARNING, "Body: {}".format(util.xxd_oneline(pkt_body)))
             return
-        json_dict = {}
+        json_dict = {"layers": []}
+
         for layer in range(num_layers):
-            layer_key = f"layer_{layer}"
             meas_carrier_list_struct = namedtuple('QcDiagNrMl1Packet', 'raster_arfcn num_cells serv_cell_index serv_cell_pci serv_ssb null_0 serv_rsrp_rx_0 serv_rsrp_rx_1 serv_rx_beam_0 serv_rx_beam_1 serv_rfic_id null_1 serv_subarr_0 serv_subarr_1')
             meas_carrier_list = meas_carrier_list_struct._make(struct.unpack('<IBBHB3sIIHHH2sHH', pkt_body[current_offset:current_offset+32]))
             current_offset += 32
-            if self.parent.json:
-                json_dict[layer_key] = {
+            layer_dict = {
                     "Layer": layer,
                     "NR-ARFCN": meas_carrier_list.raster_arfcn,
                     "SCell_PCI": meas_carrier_list.serv_cell_pci,
@@ -92,12 +91,13 @@ class DiagNrLogParser:
                     "RX_beam_0": meas_carrier_list.serv_rx_beam_0 if meas_carrier_list.serv_rx_beam_0 != 0xffff else 'NA',
                     "RX_beam_1": meas_carrier_list.serv_rx_beam_1 if meas_carrier_list.serv_rx_beam_1 != 0xffff else 'NA',
                     "Num_Cells": meas_carrier_list.num_cells,
-                    "SCell_Index": meas_carrier_list.serv_cell_index
+                    "SCell_Index": meas_carrier_list.serv_cell_index,
+                    "cells": []
                 }
-            else:
-                stdout += "Layer {}: NR-ARFCN {}, SCell PCI {:4d}/SSB {}, RSRP {:.2f}/{:.2f}, RX beam {}/{}, Num Cells: {} (S: {})\n".format(
-                    layer, meas_carrier_list.raster_arfcn, meas_carrier_list.serv_cell_pci, meas_carrier_list.serv_ssb & 0xf,
-                    self.parse_float_q7(meas_carrier_list.serv_rsrp_rx_0), self.parse_float_q7(meas_carrier_list.serv_rsrp_rx_1),
+
+            stdout += "Layer {}: NR-ARFCN {}, SCell PCI {:4d}/SSB {}, RSRP {:.2f}/{:.2f}, RX beam {}/{}, Num Cells: {} (S: {})\n".format(
+                layer, meas_carrier_list.raster_arfcn, meas_carrier_list.serv_cell_pci, meas_carrier_list.serv_ssb & 0xf,
+                      self.parse_float_q7(meas_carrier_list.serv_rsrp_rx_0), self.parse_float_q7(meas_carrier_list.serv_rsrp_rx_1),
                     meas_carrier_list.serv_rx_beam_0 if meas_carrier_list.serv_rx_beam_0 != 0xffff else 'NA',
                     meas_carrier_list.serv_rx_beam_1 if meas_carrier_list.serv_rx_beam_1 != 0xffff else 'NA',
                     meas_carrier_list.num_cells, meas_carrier_list.serv_cell_index)
@@ -111,32 +111,31 @@ class DiagNrLogParser:
                 num_cells = meas_carrier_list.num_cells
 
             for cell in range(num_cells):
-                cell_key = f"cell_{cell}"
                 cell_list_struct = namedtuple('QcDiagNrMl1Packet', 'pci pbch_sfn num_beams null_0 cell_quality_rsrp cell_quality_rsrq')
                 cell_list = cell_list_struct._make(struct.unpack('<HHB3sII', pkt_body[current_offset:current_offset+16]))
                 current_offset += 16
-                if self.parent.json:
-                    json_dict[layer_key][cell_key] = {
+                cell_dict = {
+                        "cell": cell,
                         "PCI": cell_list.pci,
                         "PBCH_SFN": cell_list.pbch_sfn,
                         "RSRP": self.parse_float_q7(cell_list.cell_quality_rsrp),
                         "RSRQ": self.parse_float_q7(cell_list.cell_quality_rsrq),
-                        "Num_Beams": cell_list.num_beams
+                        "Num_Beams": cell_list.num_beams,
+                        "beams": []
                     }
-                else:
-                    stdout += "└── Cell {}: PCI {:4d}, PBCH SFN {}, RSRP: {:.2f}, RSRQ: {:.2f}, Num Beams: {}\n".format(
+
+                stdout += "└── Cell {}: PCI {:4d}, PBCH SFN {}, RSRP: {:.2f}, RSRQ: {:.2f}, Num Beams: {}\n".format(
                         cell, cell_list.pci, cell_list.pbch_sfn,
                         self.parse_float_q7(cell_list.cell_quality_rsrp),
                         self.parse_float_q7(cell_list.cell_quality_rsrq),
                         cell_list.num_beams)
                 for beam in range(cell_list.num_beams):
-                    beam_key = f"beam_{beam}"
                     beam_meas_struct = namedtuple('QcDiagNrMl1Packet', 'ssb_index null_0 rx_beam_0 rx_beam_1 null_1 ssb_ref_timing rx_beam_info_rsrp_0 rx_beam_info_rsrp_1 nr2nr_filtered_beam_rsrp_l3 nr2nr_filtered_beam_rsrq_l3 l_2_nr_filtered_tx_beam_rsrp_l3 l_2_nr_filtered_tx_beam_rsrq_l3')
                     beam_meas = beam_meas_struct._make(struct.unpack('<HHHHIQIIIIII', pkt_body[current_offset: current_offset+44]))
                     current_offset+=44
 
-                    if self.parent.json:
-                        json_dict[layer_key][cell_key][beam_key] = {
+                    beam_dict = {
+                            "beam": beam,
                             "SSB_Index": beam_meas.ssb_index,
                             "RX_Beam_0": beam_meas.rx_beam_0,
                             "RX_Beam_1": beam_meas.rx_beam_1,
@@ -147,15 +146,17 @@ class DiagNrLogParser:
                             "Filtered_RSRP_L2Nr": self.parse_float_q7(beam_meas.l_2_nr_filtered_tx_beam_rsrp_l3),
                             "Filtered_RSRQ_L2Nr": self.parse_float_q7(beam_meas.l_2_nr_filtered_tx_beam_rsrq_l3)
                         }
-                    else:
-                        stdout += "    └── Beam {}: SSB[{}] Beam ID {}/{}, RSRP {:.2f}/{:.2f}, Filtered RSRP/RSRQ (Nr2Nr) {:.2f}/{:.2f}, Filtered RSRP/RSRQ (L2Nr) {:.2f}/{:.2f}\n".format(
-                            beam, beam_meas.ssb_index,
+                    cell_dict["beams"].append(beam_dict)
+                    stdout += "    └── Beam {}: SSB[{}] Beam ID {}/{}, RSRP {:.2f}/{:.2f}, Filtered RSRP/RSRQ (Nr2Nr) {:.2f}/{:.2f}, Filtered RSRP/RSRQ (L2Nr) {:.2f}/{:.2f}\n".format(
+                        beam, beam_meas.ssb_index,
                             beam_meas.rx_beam_0, beam_meas.rx_beam_1,
                             self.parse_float_q7(beam_meas.rx_beam_info_rsrp_0), self.parse_float_q7(beam_meas.rx_beam_info_rsrp_1),
                             self.parse_float_q7(beam_meas.nr2nr_filtered_beam_rsrp_l3), self.parse_float_q7(beam_meas.nr2nr_filtered_beam_rsrq_l3),
                             self.parse_float_q7(beam_meas.l_2_nr_filtered_tx_beam_rsrp_l3), self.parse_float_q7(beam_meas.l_2_nr_filtered_tx_beam_rsrq_l3),
                         )
 
+                layer_dict["cells"].append(cell)
+            json_dict["layers"].append(layer_dict)
         pkt_ts = util.parse_qxdm_ts(pkt_header.timestamp)
         if self.parent.json:
             json_dict['timestamp'] = time.time_ns()
